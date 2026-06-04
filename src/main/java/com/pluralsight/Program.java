7ca4bbb9ea9fc0122d9fe650cca632dbda9602fd
Michael Okeke <michaelokeke123@gmail.com>
2026-06-04 15:06:01 -0500

    Implement reading and printing product data

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -2,6 +2,11 @@ package com.pluralsight;
 
 import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Program {
     public static void main(String[] args) {
         BasicDataSource dataSource = new BasicDataSource();
@@ -9,5 +14,24 @@ public class Program {
         dataSource.setUsername("root");
         dataSource.setPassword("");
 
+        String sql = """
+                SELECT ProductId,
+                       ProductName
+                FROM products
+                """;
+
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()){
+                int ProductId = resultSet.getInt("ProductId");
+                String productName = resultSet.getString("ProductName");
+                System.out.printf("%d %s", ProductId, productName);
+            }
+        } catch (SQLException e){
+            System.out.println("Faied to retrieve products. Please try again.");
+            e.printStackTrace();
+        }
     }
 }
